5cdce32cf0cbcf867efc59ecb5c5424cf1658301
JHON JAIRO RENGIFO ASPAJO <jrengifo@gmail.com>
2026-08-17 13:09:42 -0500

    UI: prevent horizontal layout shift and zooming on mobile

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,7 @@ export default function RootLayout({ children }) {
     <html lang="es">
       <head>
         <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
       </head>
       <body>
         {children}
